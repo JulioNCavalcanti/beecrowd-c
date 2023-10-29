27ebd3283620de621bd84612a733f2ad7df95efc
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-10-29 13:26:59 -0300

    Extremamente Básico

---
+++ beecrowd_1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int soma = a + b;
+
+    printf("X = %d", soma);
+    return 0;
+}
