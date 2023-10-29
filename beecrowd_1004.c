5091e83eb779f5f215dc8e615566b6df79d4a185
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-10-29 14:56:01 -0300

    Produto Simples

---
+++ beecrowd_1004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    int prod = x * y;
+
+    printf("PROD = %d\n", prod);
+
+    return 0;
+}
